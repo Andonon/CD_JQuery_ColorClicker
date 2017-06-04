f7c7ce3ab469445b6ebe854079ff218a5ba96b9d
Troy Center <troycenter1@gmail.com>
2017-06-04 07:58:09 -0700

    Added traversing statements to change the colors. fixed bug with space that existed between the small boxes. Done with assignment.

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -1,22 +1,26 @@
 "use strict";
 function random_color(){
     var rgb = ['a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9'];
-    color = '#';  //this is what we'll return!
+    var color = '#';  //this is what we'll return!
     for(var i = 0; i < 6; i++){
-        x = Math.floor((Math.random()*16));
+        var x = Math.floor((Math.random()*16));
         color += rgb[x]; 
     }
 return color;
 }
 $(document).ready(function(){
     $('#large_box').click(function(){
-        alert('you clicked the big box!');  
+        //alert('you clicked the big box!');  
         //comment this out when you have figured out what event.stopPropagation is used for
+        $(this).css("background-color", random_color);
+        $(this).children().css("background-color", random_color);
     });
     $('.side_box').click(function(event){
+        $(this).siblings().css("background-color", random_color);
         event.stopPropagation();
     });
     $('.middle_box').click(function(event){
+        $(this).parent().css("background-color", random_color);
         event.stopPropagation();
     });
 });
